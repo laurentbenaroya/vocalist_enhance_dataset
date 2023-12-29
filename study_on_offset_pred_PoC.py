4681f4e7b19bda55fbdac7ef855a4011d70c5049
benaroya <laurent.benaroya@gmail.com>
2023-12-29 17:36:35 +0100

    many changes to make things workgit add study_on_offset_pred_PoC.py!

--- study_on_offset_pred_PoC.py
+++ study_on_offset_pred_PoC.py
@@ -21,7 +21,7 @@ from sklearn.metrics import f1_score
 torch.multiprocessing.set_sharing_strategy('file_system')
 
 from utils import load_checkpoint, force_cudnn_initialization, set_parameters
-from custom_datasets import Dataset as MyDataset
+from custom_datasets import Dataset as MyDataset, collate_fn
 
 """
 training based on sync error
@@ -46,140 +46,197 @@ print('use_cuda: {}'.format(use_cuda))
 force_cudnn_initialization()
 
 v_context = 5
-BATCH_SIZE = 1
+BATCH_SIZE = 4
+TEST_BATCH_SIZE = 4
+# optim
+learning_rate = 1e-2  # THIS MIGHT BE AN ISSUE
+weight_decay = 0.1
+# windowing
 n_classes = 8
-n_max = n_classes*4
-n_samples = 25
-delta_audio = 10  # frames offset
+n_max = n_classes*20
+# test location at audio rate (80 fps)
+audio_location = sorted([0, 3, 5, 10, 20, 30, 50, 100])  # custom
+# audio_location = np.linspace(0, n_max, n_classes)
+assert len(audio_location) == n_classes
+
+n_samples_img = 25  # nombre d'images dans les extraits
+
+audio_fps = 16000 / hparams.hop_size  # float, 80.
+video_fps = hparams.fps  # 25.
+
+n_samples_audio = int(np.floor(n_samples_img * audio_fps / video_fps))  # nombre de trames pour le melspec
+delta_audio = 10  # frames offset, UNUSED
+
+loss_fct = torch.nn.CrossEntropyLoss(reduction='sum')
 
 
 def trainit(device, model, train_data_loader, val_data_loader, optimizer, 
             checkpoint_dir, checkpoint_interval, n_epochs=10):
 
-    audio_fps = 16000/hparams.hop_size  # float, 80.
-    video_fps = hparams.fps  # 25.
-    mel_step_size = int(v_context / video_fps * audio_fps)-1  # 16
-    loss_fct = torch.nn.CrossEntropyLoss()
-    model.train()
-
-    onehot_targets = torch.nn.functional.one_hot(torch.tensor([0, 1, 2]), num_classes=n_classes).float().squeeze(0).to(
-        device)
+    onehot_targets = torch.nn.functional.one_hot(torch.tensor([0, 1, 2]),
+                                                 num_classes=n_classes*BATCH_SIZE).float().squeeze(0).to(device)
     loss_weights = [1.0, 0.95, 0.93]
 
+    # initial score !!!
+    print('INITIAL SCORE')
+    valid(device, model, val_data_loader, loss_weights, delta_audio)
+    print()
+
     for epoch in range(n_epochs):
-        print('] EPOCH %d [' % epoch)
+        model.train()
+        print(':) EPOCH %d (:' % epoch)
         total_loss = 0.0
         total_corrects = 0
         total_sum = 0
         preds_list = []
-        for step, (vid, aud, lastframe) in tqdm(enumerate(train_data_loader), total=len(train_data_loader)):
+        for step, (vid_batch, aud_batch, lastframe_batch) in tqdm(enumerate(train_data_loader), total=len(train_data_loader)):
 
             optimizer.zero_grad()
+            audio_batch = []
+            img_batch = []
+            for vid, aud, lastframe in zip(vid_batch, aud_batch, lastframe_batch):
+                # print('vid size ', vid.size())  # torch.Size([1, 325, 3, 48, 96])
+                # print('aud size ', aud.size())  # torch.Size([1, 1074, 1, 80])
 
-            # lastframe = lastframe.item()
-            vid = vid.view(1, -1, 3, 48, 96)
-            # print('vid size ', vid.size())  # torch.Size([1, 325, 3, 48, 96])
-            # print('aud size ', aud.size())  # torch.Size([1, 1074, 1, 80])
-            
-            # take context along dimension 1 and batch the windows
-            aud_batch = [aud[:, i: i + int(n_samples*audio_fps/video_fps), :, :]
-                         for i in [int(item+delta_audio) for item in np.linspace(0, n_max, n_classes)]]
-            audio_batch = torch.cat(aud_batch, 0).permute(0, 2, 3, 1).to(device)
-            B = audio_batch.size(0)
-            #
-            img_batch = vid[:, delta_audio:delta_audio+n_samples, :, :, :].view(1, -1, 48, 96).repeat(B, 1, 1, 1).to(device)
+                # take context along dimension 1 and batch the window
+                # put data to gpu
+                vid = vid.view(1, -1, 3, 48, 96).to(device)
+                aud = aud.unsqueeze(0).to(device)  # cast to size 1 batch
+                # do windowing
+                # 1) audio
+                aud_bat = [aud[:, int(i): int(i) + n_samples_audio, :, :]
+                           for i in audio_location]
+                audio_bat = torch.cat(aud_bat, 0)  # n_classes x n_samples_audio x 1 x 80 (mel)
+                audio_batch.append(audio_bat)
 
-            # print()
-            # print(f'img_batch size {img_batch.size()}')
-            # print(f'audio_batch size {audio_batch.size()}')
-            # print('calc loss')
+                # 2) images
+                # B = n_classes  # audio_bat.size(0)
+                img_bat = vid[:, 0: n_samples_img, :, :, :]  # 1 x n_samples_img x 3 x 48 x 96 (img crop)
+                img_bat = img_bat.view(1, -1, 48, 96)  # fuse n_samples_img and RGB channels
+                img_bat = img_bat.repeat(n_classes, 1, 1, 1)  # repeat on dim 0, n_classes x n_samples_img*3, x 48 x 96
+                img_batch.append(img_bat)
+
+                # print(f'img_bat size {img_bat.size()}')
+                # print(f'audio_bat size {audio_bat.size()}')
+            # final data, fuse on first dimension
+            audio_batch = torch.stack(audio_batch, 0)  # create a new dimension with batch_size
+            img_batch = torch.stack(img_batch, 0)  # create a new dimension with batch_size
+            # group BATCH_SIZE with n_classes (dim 0 and 1)
+            img_batch = img_batch.view(BATCH_SIZE*n_classes, n_samples_img*3, 48, 96)
+            audio_batch = audio_batch.view(BATCH_SIZE*n_classes, n_samples_audio, 1, 80)   #
+            # IL PEUT Y AVOIR UN PROBLEME ICI AUSSI
+            audio_batch = audio_batch.permute(0, 2, 3, 1)  # .permute(0, 2, 1, 3)
+            # print('audio_batch.size() ', audio_batch.size())
+
+            # go through model
             raw_sync_scores = model(img_batch, audio_batch)
-            # print('raw_sync_scores.size() ', raw_sync_scores.size())
-            # print(torch.nn.functional.softmax(raw_sync_scores, 0))
+
+            # loss
             loss = torch.tensor([0.], device=device)
             for v, w in enumerate(loss_weights):
                 loss += w*loss_fct(raw_sync_scores, onehot_targets[v])
             # print('loss = ', loss)
 
+            # gradient descent
             loss.backward()
             optimizer.step()
+
+            # indicators
             total_loss += loss.item()
-            preds = torch.argmax(raw_sync_scores).item()
+            # IL PEUT Y AVOIR UN PROBLEM ICI
+            preds = torch.argmax(raw_sync_scores.view(BATCH_SIZE, n_classes), 1).clone().cpu().numpy()
             # print(torch.nn.functional.softmax(raw_sync_scores))
-            # print(preds.item())
+
+            # print('preds.shape ', preds.shape)
             for v in range(len(loss_weights)):
                 total_corrects += np.sum(preds == v)
-            total_sum += 1
+            total_sum += BATCH_SIZE
             preds_list.append(preds)
-            # Add these lines to obtain f1_score
 
-        # if step % n_step == 0:    # print
-        preds_np = np.array(preds_list)
-        f1_score_out = f1_score(np.zeros_like(preds_np), preds_np, average='micro')
+        # Add these lines to obtain f1_score
+        # preds_np = np.array(preds_list)
+        # print(preds_np.shape)
+        # preds_np = preds_np.flatten()
+        # print(preds_np.shape)
+        # f1_score_out = f1_score(np.zeros_like(preds_np), preds_np, average='micro')
         # print(f1_score_out)
-        print(f'[TRAIN], loss: {total_loss / total_sum:.3f}, acc: {total_corrects/ total_sum*100.:.1f}')
+
+        # I'm not sure about the multiplication by BATCH_SIZE
+        print(f'[TRAIN], loss: {total_loss / total_sum*BATCH_SIZE:.3f}, acc: {total_corrects/ total_sum*100.:.1f}')
         # , f1 score: {f1_score_out*100.:.1f}')
 
-        # if step % n_valid_step == 0:
-        valid(device, model, val_data_loader, onehot_targets, loss_weights, delta_audio)
+        valid(device, model, val_data_loader, loss_weights, delta_audio)
+        print()
 
 
-def valid(device, model, val_data_loader, onehot_targets, loss_weights, delta_audio):
+def valid(device, model, val_data_loader, loss_weights, delta_audio):
 
     audio_fps = 16000/hparams.hop_size  # float, 80.
     video_fps = hparams.fps  # 25.
     mel_step_size = int(v_context / video_fps * audio_fps)-1  # 16
-    loss_fct = torch.nn.CrossEntropyLoss()
+    onehot_targets = torch.nn.functional.one_hot(torch.tensor([0, 1, 2]),
+                                                 num_classes=n_classes*TEST_BATCH_SIZE).float().squeeze(0).to(device)
     model.eval()
 
     total_loss = 0.0
     preds_list = []
     total_corrects = 0
     total_elts = 0
-    # n_max_step = len(val_data_loader)//4-1
-    for step, (vid, aud, lastframe) in tqdm(enumerate(val_data_loader), total=len(val_data_loader)):
+    for step, (vid_batch, aud_batch, lastframe_batch) in tqdm(enumerate(val_data_loader), total=len(val_data_loader)):
+        audio_batch = []
+        img_batch = []
         with torch.no_grad():
+            for vid, aud, lastframe in zip(vid_batch, aud_batch, lastframe_batch):
+                # print('vid size ', vid.size())  # torch.Size([1, 325, 3, 48, 96])
+                # print('aud size ', aud.size())  # torch.Size([1, 1074, 1, 80])
 
-            # print('vid size ', vid.size())  # torch.Size([1, 325, 3, 48, 96])
-            # print('aud size ', aud.size())  # torch.Size([1, 1074, 1, 80])
+                # take context along dimension 1 and batch the window
+                # put data to gpu
+                vid = vid.view(1, -1, 3, 48, 96).to(device)
+                aud = aud.unsqueeze(0).to(device)  # cast to size 1 batch
+                # do windowing
+                # 1) audio
+                aud_bat = [aud[:, int(i): int(i) + n_samples_audio, :, :]
+                           for i in audio_location]
+                audio_bat = torch.cat(aud_bat, 0)  # n_classes x n_samples_audio x 1 x 80 (mel)
+                audio_batch.append(audio_bat)
 
-            # lastframe = lastframe.item()
-            vid = vid.view(1, -1, 3, 48, 96)
+                # 2) images
+                # B = n_classes  # audio_bat.size(0)
+                img_bat = vid[:, 0: n_samples_img, :, :, :]  # 1 x n_samples_img x 3 x 48 x 96 (img crop)
+                img_bat = img_bat.view(1, -1, 48, 96)  # fuse n_samples_img and RGB channels
+                img_bat = img_bat.repeat(n_classes, 1, 1, 1)  # repeat on dim 0, n_classes x n_samples_img*3, x 48 x 96
+                img_batch.append(img_bat)
 
-            # take context along dimension 1 and batch the windows
-            aud_batch = [aud[:, i: i + int(n_samples*audio_fps/video_fps), :, :]
-                         for i in [int(item+delta_audio) for item in np.linspace(0, n_max, n_classes)]]
-            audio_batch = torch.cat(aud_batch, 0).permute(0, 2, 3, 1).to(device)
-            B = audio_batch.size(0)
-            #
-            img_batch = vid[:, delta_audio:delta_audio+n_samples, :, :, :].view(1, -1, 48, 96).repeat(B, 1, 1, 1).to(device)
-
-            # im_batch = [vid[:, i: i + n_samples, :, :, :].view(1, -1, 48, 96)
-            #             for i in [int(item+delta_audio) for item in np.linspace(0, n_max, n_classes)]]
-            # img_batch = torch.cat(im_batch, 0).to(device)
-            # B = img_batch.size(0)
-            # audio_batch = aud[:, :int(n_samples*audio_fps/video_fps), :, :].repeat(B, 1, 1, 1).permute(0, 2, 3, 1).to(device)
-
+                # print(f'img_bat size {img_bat.size()}')
+                # print(f'audio_bat size {audio_bat.size()}')
+                # final data, fuse on first dimension
+            audio_batch = torch.stack(audio_batch, 0)  # create a new dimension with batch_size
+            img_batch = torch.stack(img_batch, 0)  # create a new dimension with batch_size
+            # group BATCH_SIZE with n_classes (dim 0 and 1)
+            img_batch = img_batch.view(BATCH_SIZE * n_classes, n_samples_img * 3, 48, 96)
+            audio_batch = audio_batch.view(BATCH_SIZE * n_classes, n_samples_audio, 1, 80)  #
+            # IL PEUT Y AVOIR UN PROBLEME ICI AUSSI
+            audio_batch = audio_batch.permute(0, 2, 3, 1)  # .permute(0, 2, 1, 3) -> the old one
             raw_sync_scores = model(img_batch, audio_batch)
+
             loss = torch.tensor([0.], device=device)
             for v, w in enumerate(loss_weights):
                 loss += w * loss_fct(raw_sync_scores, onehot_targets[v])
 
-        preds = torch.argmax(raw_sync_scores).item()
+        preds = torch.argmax(raw_sync_scores.view(TEST_BATCH_SIZE, n_classes), 1).clone().cpu().numpy()
         preds_list.append(preds)
 
         for v in range(len(loss_weights)):
             total_corrects += np.sum(preds == v)
-        total_elts += 1
+        total_elts += TEST_BATCH_SIZE
         total_loss += loss.item()
 
-        # if step == n_max_step:
-        #     break
+    # preds_np = np.array(preds_list)
+    # f1_score_out = f1_score(np.zeros_like(preds_np), preds_np, average='micro')
 
-    preds_np = np.array(preds_list)
-    f1_score_out = f1_score(np.zeros_like(preds_np), preds_np, average='micro')
-
-    print(f'[VALIDATION], loss: {total_loss / total_elts:.3f},'
+    # I'm not sure about the multiplication by TEST_BATCH_SIZE
+    print(f'[VALIDATION], loss: {total_loss / total_elts*TEST_BATCH_SIZE:.3f},'
           f' acc: {total_corrects/ total_elts*100.:.1f}')  # , f1 score: {f1_score_out*100.:.1f}')
 
 
@@ -195,15 +252,18 @@ if __name__ == "__main__":
     val_dataset = MyDataset('val', overwrite=True, device='cpu')
 
     print('Data loader setup')
-    train_data_loader = data_utils.DataLoader(train_dataset, batch_size=1,  num_workers=2, shuffle=True)
-    val_data_loader = data_utils.DataLoader(val_dataset, batch_size=1, num_workers=2)
+    train_data_loader = data_utils.DataLoader(train_dataset, batch_size=BATCH_SIZE,  num_workers=4, shuffle=True,
+                                              collate_fn=collate_fn, drop_last=True)
+    val_data_loader = data_utils.DataLoader(val_dataset, batch_size=TEST_BATCH_SIZE, num_workers=2, shuffle=False,
+                                            collate_fn=collate_fn, drop_last=True)
 
     # Model
     print('build model')
     model = SyncTransformer().to(device)
     params = set_parameters(model)
-    optimizer = optim.Adam(params, lr=1e-4)
-
+    # optimizer = optim.Adam(params, lr=learning_rate)
+    # AdamW !!!
+    optimizer = optim.AdamW(params, lr=learning_rate, betas=(0.9, 0.99), weight_decay=weight_decay)
     if checkpoint_path is not None:
         print('load checkpoint')
         load_checkpoint(checkpoint_path, model, optimizer, reset_optimizer=True, use_cuda=use_cuda)
@@ -212,4 +272,4 @@ if __name__ == "__main__":
         global_epoch = 0
 
     trainit(device, model, train_data_loader, val_data_loader, optimizer, checkpoint_dir=checkpoint_dir,
-            checkpoint_interval=10, n_epochs=650)
+            checkpoint_interval=-1, n_epochs=5)
